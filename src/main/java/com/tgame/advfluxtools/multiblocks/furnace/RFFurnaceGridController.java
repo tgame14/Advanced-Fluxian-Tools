f805187047f9bb171a3545345b560835b0274ef4
tgame14 <tgameBusiness@gmail.com>
2014-06-10 16:13:21 +0300

    adding RFFurnaceGridController

---
+++ src/main/java/com/tgame/advfluxtools/multiblocks/furnace/RFFurnaceGridController.java
@@ -0,0 +1,18 @@
+package com.tgame.advfluxtools.multiblocks.furnace;
+
+import com.tgame.advfluxtools.multiblocks.energy.RFGridController;
+import net.minecraft.world.World;
+
+/**
+ * @author tgame14
+ * @since 04/06/14
+ */
+public class RFFurnaceGridController extends RFGridController
+{
+	public RFFurnaceGridController(World world)
+	{
+		super(world);
+	}
+
+
+}
