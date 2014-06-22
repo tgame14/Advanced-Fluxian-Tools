982eeabaae63bc57bd1a6539685dc9a0c27aaa25
tgame14 <tgameBusiness@gmail.com>
2014-06-22 18:36:07 +0300

    Broken furnace, requires fixing, close though

--- src/main/java/com/tgame/advfluxtools/multiblocks/furnace/RFFurnaceGridController.java
+++ src/main/java/com/tgame/advfluxtools/multiblocks/furnace/RFFurnaceGridController.java
@@ -1,10 +1,13 @@
 package com.tgame.advfluxtools.multiblocks.furnace;
 
 import com.tgame.advfluxtools.multiblocks.energy.RFGridController;
+import com.tgame.mods.config.Config;
 import com.tgame.mods.libs.inventory.IInventoryHandler;
-import com.tgame.mods.libs.machinery.Furnace;
+import com.tgame.mods.libs.inventory.simpleimpl.InventoryStorage;
 import com.tgame.mods.libs.multiblocks.MultiblockValidationException;
+import com.tgame.mods.libs.multiblocks.grid.AbstractMultiblockNode;
 import net.minecraft.item.ItemStack;
+import net.minecraft.item.crafting.FurnaceRecipes;
 import net.minecraft.nbt.NBTTagCompound;
 import net.minecraft.util.StatCollector;
 import net.minecraft.world.World;
@@ -18,13 +21,28 @@ import java.util.List;
  */
 public class RFFurnaceGridController extends RFGridController implements IInventoryHandler
 {
-	protected Furnace furnace;
+	@Config
+	public static int ENERGY_PER_ITEM_IN_FURNACE = 2000;
+
+	protected int burnTimePerItem;
+	protected int currentBurnTime;
+	protected int costPerTick;
+	protected boolean isBurning;
+
+	protected InventoryStorage input;
+	protected InventoryStorage output;
 
 	public RFFurnaceGridController(World world)
 	{
 		super(world);
 
-		this.furnace = new Furnace(0, 1, 1);
+		this.burnTimePerItem = 0;
+		this.currentBurnTime = 0;
+
+		this.input = new InventoryStorage(0);
+		this.output = new InventoryStorage(0);
+		this.isBurning = false;
+
 	}
 
 	public int calcInternalSpace()
@@ -33,11 +51,11 @@ public class RFFurnaceGridController extends RFGridController implements IInvent
 		int maxY = this.getMaximumCoord().y();
 		int maxZ = this.getMaximumCoord().z();
 
-		int minX = this.getMaximumCoord().x();
-		int minY = this.getMaximumCoord().y();
-		int minZ = this.getMaximumCoord().z();
+		int minX = this.getMinimumCoord().x();
+		int minY = this.getMinimumCoord().y();
+		int minZ = this.getMinimumCoord().z();
 
-		return (maxX - minX - 2) * (maxZ - minZ - 2) * (maxY - minY - 2);
+		return (maxX - minX - 1) * (maxZ - minZ - 1) * (maxY - minY - 1);
 
 	}
 
@@ -46,9 +64,76 @@ public class RFFurnaceGridController extends RFGridController implements IInvent
 	{
 		if (this.isAssembled())
 		{
-			return furnace.updateFurnace();
+			if (this.getEnergyStored(ForgeDirection.UNKNOWN) >= this.costPerTick)
+			{
+				if (!isBurning)
+				{
+					for (AbstractMultiblockNode node : this.connectedParts)
+					{
+						node.onMachineActivated();
+					}
+					this.isBurning = true;
+
+				}
+				if (this.currentBurnTime > 0)
+				{
+					if (canSmelt())
+					{
+						this.extractEnergy(ForgeDirection.UNKNOWN, this.costPerTick, false);
+						this.currentBurnTime--;
+						return true;
+					}
+
+				}
+				else
+				{
+					ItemStack smelted = null;
+					for (int i = 0; i < this.input.getSizeInventory(); i++)
+					{
+						if (this.input.getInventoryContents().get(i) != null)
+						{
+							smelted = this.input.getInventoryContents().get(i);
+						}
+					}
+
+					ItemStack out = FurnaceRecipes.smelting().getSmeltingResult(smelted);
+					this.input.extractItem(smelted, false);
+					this.output.insertItem(out, false);
+					return true;
+				}
+
+			}
 		}
+		if (this.isBurning)
+		{
+			for (AbstractMultiblockNode node : this.connectedParts)
+			{
+				node.onMachineDeactivated();
+			}
+			this.isBurning = false;
+		}
+
 		return false;
+	}
+
+	private boolean canSmelt()
+	{
+		ItemStack candidate = this.input.getInventoryContents().get(0);
+		if (candidate == null)
+			return false;
+
+		ItemStack stack = FurnaceRecipes.smelting().getSmeltingResult(candidate);
+		if (stack == null)
+		{
+			return false;
+		}
+		ItemStack out = this.output.insertItem(stack.copy(), true);
+		if (out.stackSize == stack.stackSize)
+		{
+			return false;
+		}
+		return true;
+
 	}
 
 	@Override
@@ -64,15 +149,21 @@ public class RFFurnaceGridController extends RFGridController implements IInvent
 	protected void onMachineAssembled()
 	{
 		super.onMachineAssembled();
-		this.furnace.setActive(true);
-		this.furnace.setBurnTicksPerItem(5 / this.calcInternalSpace());
+
+		int internalSpace = this.calcInternalSpace();
+
+		this.input.setMaxSlots(internalSpace * 2);
+		this.output.setMaxSlots(internalSpace * 2);
+
+		this.burnTimePerItem = 175 / internalSpace;
+		this.currentBurnTime = burnTimePerItem;
+		this.costPerTick = ENERGY_PER_ITEM_IN_FURNACE / this.burnTimePerItem;
 	}
 
 	@Override
 	protected void onMachineDisassembled()
 	{
 		super.onMachineDisassembled();
-		this.furnace.setActive(false);
 	}
 
 	@Override
@@ -108,54 +199,62 @@ public class RFFurnaceGridController extends RFGridController implements IInvent
 	@Override
 	public ItemStack insertItem(ForgeDirection from, ItemStack item, boolean simulate)
 	{
-		return this.furnace.getInputInv().insertItem(item, simulate);
+		return this.input.insertItem(item, simulate);
 	}
 
 	@Override
 	public ItemStack extractItem(ForgeDirection from, ItemStack item, boolean simulate)
 	{
-		return this.furnace.getOutputInv().extractItem(item, simulate);
+		return this.output.extractItem(item, simulate);
 	}
 
 	@Override
 	public ItemStack extractItem(ForgeDirection from, int maxExtract, boolean simulate)
 	{
-		return this.furnace.getOutputInv().extractItem(maxExtract, simulate);
+		return this.output.extractItem(maxExtract, simulate);
 	}
 
 	@Override
 	public List<ItemStack> getInventoryContents(ForgeDirection from)
 	{
-		return this.furnace.getOutputInv().getInventoryContents();
+		return this.output.getInventoryContents();
 	}
 
 	@Override
 	public int getSizeInventory(ForgeDirection from)
 	{
-		return this.furnace.getOutputInv().getSizeInventory();
+		return this.output.getSizeInventory();
 	}
 
 	@Override
 	public boolean isEmpty(ForgeDirection from)
 	{
-		return this.furnace.getInputInv().isEmpty();
+		return this.input.isEmpty();
 	}
 
 	@Override
 	public boolean isFull(ForgeDirection from)
 	{
-		return this.furnace.getInputInv().isFull();
+		return this.input.isFull();
 	}
 
 	@Override
 	public void writeToNBT(NBTTagCompound data)
 	{
+		System.out.println("isBurning = " + isBurning);
+		System.out.println("burnTimePerItem = " + burnTimePerItem);
+		System.out.println("currentBurnTime = " + currentBurnTime);
+		System.out.println("energy = " + energy);
 		super.writeToNBT(data);
 
-		NBTTagCompound furnaceTag = new NBTTagCompound();
-		this.furnace.writeToNBT(furnaceTag);
+		NBTTagCompound inputTag = new NBTTagCompound();
+		this.input.writeToNBT(inputTag);
 
-		data.setTag("furnaceTag", furnaceTag);
+		NBTTagCompound outputTag = new NBTTagCompound();
+		this.output.writeToNBT(outputTag);
+
+		data.setTag("inputTag", inputTag);
+		data.setTag("outputTag", outputTag);
 	}
 
 	@Override
@@ -163,27 +262,19 @@ public class RFFurnaceGridController extends RFGridController implements IInvent
 	{
 		super.readFromNBT(data);
 
-		this.furnace.readFromNBT(data.getCompoundTag("furnaceTag"));
+		this.input.readFromNBT(data.getCompoundTag("inputTag"));
+		this.output.readFromNBT(data.getCompoundTag("outputTag"));
 	}
 
 	@Override
 	public int receiveEnergy(ForgeDirection from, int maxReceive, boolean simulate)
 	{
-		if (this.getEnergyStored(ForgeDirection.UNKNOWN) == 0 && !simulate && maxReceive > 0)
-		{
-			this.furnace.setHasFuel(true);
-		}
 		return super.receiveEnergy(from, maxReceive, simulate);
 	}
 
 	@Override
 	public int extractEnergy(ForgeDirection from, int maxExtract, boolean simulate)
 	{
-
-		if (this.getEnergyStored(ForgeDirection.UNKNOWN) > 0 && !simulate && maxExtract >= this.getEnergyStored(ForgeDirection.UNKNOWN) )
-		{
-			this.furnace.setHasFuel(false);
-		}
 		return super.extractEnergy(from, maxExtract, simulate);
 	}
 }
